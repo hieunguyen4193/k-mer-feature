11cb79b17aa0f12dd7cbd3e0ed8dfa00d5e55e92
Trong-Hieu Nguyen <work@hieunguyen.de>
2025-09-18 20:26:47 +0200

    update fix bugs

--- filter_BAM.sh
+++ filter_BAM.sh
@@ -4,14 +4,14 @@ bedname=${bedname%.bed*};
 outputdir="/mnt/DATASM14/DATA_HIEUNGUYEN/outdir/k-mer-feature";
 mkdir -p ${outputdir}/${bedname};
 
-all_input_bams=$(cat full_metadata.csv | cut -d, -f4);
+all_input_bams=$(cat full_metadata.csv | tail -n +2 | cut -d, -f4);
 
 for inputbam in ${all_input_bams};do \
     filename=$(echo $inputbam | xargs -n 1 basename);
     echo -e "working on file "  $filename;
 
     ##### filter bam file with FULL BED FILE
-    samtools view -b -h -L ${inputbed} ${inputbam} > ${outputdir}/${bedname}/${filename%.bam*}.filtered.bam;
+    samtools view -b -h ${inputbam} -L ${inputbed} > ${outputdir}/${bedname}/${filename%.bam*}.filtered.bam;
     samtools index ${outputdir}/${bedname}/${filename%.bam*}.filtered.bam;
 
     ##### filter BAM file with each region in the bed file
@@ -23,7 +23,7 @@ for inputbam in ${all_input_bams};do \
         echo -e "working on region " $regionname;
         echo -e "coordinate of regions: " $region;
         mkdir -p ${outputdir}/${bedname}/${regionname}
-        samtools view -b -h -L $region ${inputbam} > ${outputdir}/${bedname}/${regionname}/${filename%.bam*}.filtered.bam;
+        samtools view -b -h ${inputbam} -L $region  > ${outputdir}/${bedname}/${regionname}/${filename%.bam*}.filtered.bam;
         samtools index ${outputdir}/${bedname}/${regionname}/${filename%.bam*}.filtered.bam;
     done;
 done;
